141180b4779466256fc5ba4d7b14f645f51bd961
vincemilo <vincemilo@gmail.com>
2021-12-23 12:16:15 -0700

    basic unit movement and created rspec for future testing

--- units/pawn.rb
+++
@@ -1,7 +0,0 @@
-class Pawn
-  attr_reader :moves
-
-  def initialize
-    @moves = [[0, 1], [0, 2]]
-  end
-end
--- units/rook.rb
+++
@@ -1,8 +0,0 @@
-class Rook
-  attr_reader :moves
-  
-  def initialize
-    @moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-  end
-end
-  
