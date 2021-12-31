04df25cdf6aabaa71c51242911240d8cf19c084d
vincemilo <vincemilo@gmail.com>
2021-12-31 11:35:35 -0700

    cleaned up pawn functions, switching over to TDD as it will be easier in longrun

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative 'units/pawn'
 
 class Game
@@ -24,15 +25,15 @@ class Game
     @board[6].map! { 'p' }
   end
 
-  def move_unit(start_pos, end_pos, piece)
-    return unless valid_move?(start_pos, end_pos, piece)
+  # def move_unit(start_pos, end_pos, piece)
+  #   return unless valid_move?(start_pos, end_pos, piece)
 
-    @board[x_pos(start_pos)][y_pos(start_pos)] = '0'
-    capture(end_pos) if @board[x_pos(end_pos)][y_pos(end_pos)] != '0'
-    @board[x_pos(end_pos)][y_pos(end_pos)] = piece
-    @turn += 1
-    @turn %= 2
-  end
+  #   @board[x_pos(start_pos)][y_pos(start_pos)] = '0'
+  #   capture(end_pos) if @board[x_pos(end_pos)][y_pos(end_pos)] != '0'
+  #   @board[x_pos(end_pos)][y_pos(end_pos)] = piece
+  #   @turn += 1
+  #   @turn %= 2
+  # end
 
   def x_pos(pos)
     # extracts x coordinate
@@ -44,27 +45,27 @@ class Game
     pos[1]
   end
 
-  def capture(pos)
-    piece = @board[x_pos(pos)][y_pos(pos)]
-    if @turn.zero?
-      @captured[0] << piece
-    else
-      @captured[1] << piece
-    end
-    puts "Captured pieces: #{@captured}"
-  end
+  # def capture(pos)
+  #   piece = @board[x_pos(pos)][y_pos(pos)]
+  #   if @turn.zero?
+  #     @captured[0] << piece
+  #   else
+  #     @captured[1] << piece
+  #   end
+  #   puts "Captured pieces: #{@captured}"
+  # end
 
   def valid_move?(start_pos, end_pos, piece)
     return false if end_pos[0].negative? || end_pos[0] > 7 ||
                     end_pos[1].negative? || end_pos[1] > 7
 
-    piece = get_piece(piece, start_pos, end_pos)
-    piece.moves.each do |move|
-      puts move
-    end
-    # print "#{piece.moves} \n"
-    print "#{start_pos} \n"
-    print "#{end_pos} \n"
+    # piece = get_piece(piece)
+    # piece.moves.each do |move|
+    #   puts move
+    # end
+    # # print "#{piece.moves} \n"
+    # print "#{start_pos} \n"
+    # print "#{end_pos} \n"
     true
   end
 
@@ -74,24 +75,54 @@ class Game
     false
   end
 
-  def get_piece(piece, start_pos, end_pos)
-    white_pawn(start_pos, end_pos) if piece == 'P'
+  def enemy_occupied?(piece)
+    white_pieces = %w[P R N B Q K]
+    black_pieces = white_pieces.map(&:downcase)
+
+    return true if @turn.zero? && white_pieces.any?(piece)
+
+    return true if @turn.positive? && black_pieces.any?(piece)
+
+    false
+  end
+
+  def get_piece(square)
+    @board[y_pos(square)][x_pos(square)]
+  end
+
+  def one_ahead(start_pos)
+    @board[y_pos(start_pos) + 1][x_pos(start_pos)]
+  end
+
+  def two_ahead(start_pos)
+    @board[y_pos(start_pos) + 2][x_pos(start_pos)]
+  end
+
+  def r_diag
+    @board[y_pos(start_pos) + 1][x_pos(start_pos) + 1]
+  end
+
+  def l_diag
+    @board[y_pos(start_pos) + 1][x_pos(start_pos) - 1]
   end
 
   def white_pawn(start_pos, end_pos)
     # starting line
     pawn = Pawn.new
-    one_ahead = @board[y_pos(start_pos) + 1][x_pos(start_pos)]
-    two_ahead = @board[y_pos(start_pos) + 2][x_pos(start_pos)]
-    display_board
+
     if start_pos[1] == 1
-      pawn.moves = if one_ahead == '0' && two_ahead == '0'
+      pawn.moves = if !occupied?(one_ahead(start_pos)) &&
+                      !occupied?(two_ahead(start_pos))
                      [[0, 1], [0, 2]]
-                   elsif one_ahead == '0'
+                   elsif !occupied?(one_ahead(start_pos))
                      [[0, 1]]
                    else
                      []
                    end
+    elsif end_pos[1] == 7
+      promote(end_pos)
+    else
+      pawn.moves = [0, 1]
     end
     pawn
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
 require_relative '../lib/game'
 
 describe Game do
@@ -8,6 +12,30 @@ describe Game do
       it 'returns false' do
         expect(game.valid_move?([1, 1], [-1, 1], 'P')).to eq(false)
       end
+    end
+  end
+
+  describe '#enemy_occupied?' do
+    context 'when its\'s white\'s turn' do
+      it 'returns true if black pieces are found' do
+        game.turn = 1
+        black_piece = 'p'
+        expect(game.enemy_occupied?(black_piece)).to eq(true)
+      end
+    end
+
+    context 'when its\'s black\'s turn' do
+      it 'returns true if white pieces are found' do
+        game.turn = 0
+        white_piece = 'P'
+        expect(game.enemy_occupied?(white_piece)).to eq(true)
+      end
+    end
+  end
+
+  describe '#get_piece' do
+    it 'returns the piece at the given coordinates' do
+      expect(game.get_piece([2, 1])).to eq('P')
     end
   end
 
@@ -27,7 +55,7 @@ describe Game do
       before do
         x = 1
         y = 3
-        game.board[y][x] = 'X'
+        game.board[y][x] = 'p'
       end
 
       it 'should have one move ahead' do
@@ -43,7 +71,7 @@ describe Game do
       before do
         x = 1
         y = 2
-        game.board[y][x] = 'X'
+        game.board[y][x] = 'p'
       end
 
       it 'should have no legal moves' do
@@ -54,6 +82,21 @@ describe Game do
       end
     end
 
+    context 'when a unit is one space in front but pawn has diagonal
+             captures available' do
+      before do
+        x = 1
+        y = 2
+        game.board[y][x] = 'p'
+        display_board
+      end
 
+      xit 'should have diagonal moves' do
+        start_pos = [1, 1]
+        end_pos = [1, 2]
+        pawn = game.white_pawn(start_pos, end_pos)
+        expect(pawn.moves).to eq [[1, 1], [-1, 1]]
+      end
+    end
   end
 end
