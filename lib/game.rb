50195dd56564277981c3060d718b7bdf4abf84a9
vincemilo <vincemilo@gmail.com>
2022-01-20 10:52:43 -0700

    added #move_translator to Game to take coords from player

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,30 @@ require_relative 'board'
 require_relative 'units/pawn'
 
 class Game
-  attr_accessor
+  attr_reader :turn
 
-  def initialize; end
+  def initialize
+    @turn = 0
+  end
+
+  def intro
+    puts 'Welcome to Chess!'
+    puts "Player #{@turn + 1} please select your piece (i.e. e4):"
+    p_select = gets.chomp
+    move_translator(p_select)
+  end
+
+  def move_translator(coords)
+    row = coords[0]
+    column = coords[1]
+    row_num = row.ord - 96
+    [row_num.ord, column.to_i]
+  end
+
+  def select_unit
+
+  end
 end
+
+# game = Game.new
+# game.intro
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/board'
+require_relative '../lib/units/pawn'
 
 describe Game do
-  subject(:game) { described_class.new }
+  def display_board
+    print "#{('a'..'h').to_a} \n"
+    board.data.reverse.each do |row|
+      puts row.to_s
+    end
+    print "#{('a'..'h').to_a} \n"
+  end
+
+  describe '#move_translator' do
+    subject(:game) { described_class.new }
+
+    before do
+      allow(game).to receive(:puts)
+      allow(game).to receive(:gets)
+    end
+
+    it 'returns the coords for the board' do
+      expect(game.move_translator('e4')).to eq([5, 4])
+    end
+  end
+
+  describe '#select_unit' do
+    
+  end
 end
